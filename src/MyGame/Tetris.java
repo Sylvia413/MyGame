d7d829d91f08b930db9dcbdc9ad6ba70d964d1f8
Sylvia <fleetingtiming@163.com>
2018-03-22 21:00:29 +0800

    Commit

---
+++ src/MyGame/Tetris.java
@@ -0,0 +1,17 @@
+package MyGame;
+
+import javax.swing.JFrame;
+
+public class Tetris  
+{  
+    public static void main(String args[])  
+    {  
+        MainWindowFrame mainWindow=new MainWindowFrame();  
+        mainWindow.setTitle("Tetris");  
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        mainWindow.setVisible(true);  
+  
+    }  
+  
+  
+}  
